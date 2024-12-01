f6b53fb460dad4e3a6411c39d6d1afb0c6067182
baichuan1205 <sbc1205@126.com>
2024-12-01 14:43:34 +0800

    新更改

--- p2.py
+++ p2.py
@@ -1,6 +1,6 @@
 # 写入文件
 with open('example.txt', 'w') as file:
-    file.write("Hello, World!")
+    file.write("Hello, World!Hello, World!Hello, World!Hello, World!")
 
 # 读取文件
 with open('example.txt', 'r') as file:
